3f49adf6da9b38b827f0f6c68928b496cded3af2
Subhasish <greendoor3000@gmail.com>
2026-03-04 02:59:48 +0530

    temp

--- src/types/pubsub.rs
+++ src/types/pubsub.rs
@@ -81,7 +81,7 @@ pub enum PubsubMessage<P: Preset> {
     /// Gossipsub message providing notification of a payload attestation message.
     PayloadAttestationMessage(Arc<PayloadAttestationMessage>),
     /// Gossipsub message providing notification of an execution payload bid.
-    ExecutionPayloadBid(Arc<SignedExecutionPayloadBid>),
+    ExecutionPayloadBid(Arc<SignedExecutionPayloadBid<P>>),
 }
 
 // Implements the `DataTransform` trait of gossipsub to employ snappy compression
